341880312211a0874f32ffabd93f9a028c907ae8
Athullyamol S R <76622572+athullya-mol@users.noreply.github.com>
2023-09-22 18:39:54 +0530

    Delete dart_program1 directory

--- dart_program1/bin/calc.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-void main(){
-  print("Enter 2 numbers");
-  int a=int.parse(stdin.readLineSync()!);//input first number
-  int b=int.parse(stdin.readLineSync()!);//input second number
-  print(a+b);//Addition
-
-}
--- dart_program1/bin/dart_program1.dart
+++
@@ -1,6 +0,0 @@
-void main (){
-  int a=9;
-  int b=7;
-int c=a+b;
-  int d=a*b;
-  int
